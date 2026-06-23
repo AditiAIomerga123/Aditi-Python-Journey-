9ea58f7dc2b0590e77f636113e41420318884c99
AditiAIomerga123 <sunitajadhav7765@gmail.com>
2026-06-23 19:34:08 +0530

    Create Aditi Ai

---
+++ aditi_ai.py
@@ -0,0 +1,6 @@
+#Aditi AI-10th Project by Aditi from Umerga
+print("===ADITI AI CHATBOT===")
+name=input ("tuz naav kay Tai/dada?")
+print("namaskar",name, "me Aditi AI bolte")
+print("BCS 1st Year pasunch mi code karte!")
+print("aaj 10va project zala!Target: Google AI!")
